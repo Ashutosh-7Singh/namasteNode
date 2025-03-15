4b73d35ef8ccd72e8dcd4ac5bcea449f8627d8eb
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2025-03-15 19:31:42 +0530

    explroing http

--- namaste_node/devTinder-BE/src/app.js
+++ namaste_node/devTinder-BE/src/app.js
@@ -1,11 +1,18 @@
-const express= require("express")
-const app=express();
+const express = require("express");
+const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Heloo from test")
-})
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ashutosh", lastName: "Singh" });
+});
 
-app.listen(8888,()=>{
-    console.log("Server is listening on port 888.....");
-    
-})
+app.post("/user", (req, res) => {
+  res.send("Data Successfully saved to the database !");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Heloo fomr the server!");
+});
+
+app.listen(8888, () => {
+  console.log("Server is listening on port 8888.....");
+});
